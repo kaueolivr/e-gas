f32ba622217eebb5c2db4e85c9e946a5f4cfa60e
Kauê Patrick de Oliveira <nullguess.kaue@gmail.com>
2022-07-20 18:46:32 -0300

    Update SSL config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -6,5 +6,7 @@ module.exports = {
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT, */
-  ssl: true
+  ssl: {
+    rejectUnauthorized: false
+  }
 }
