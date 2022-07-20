6ad16a5c785dec70bb32cbab94e3fdc0f3c3fa5c
Kauê Patrick de Oliveira <nullguess.kaue@gmail.com>
2022-07-20 17:57:38 -0300

    Change the database connection method

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,9 @@
 // Configure pool to connect to the database
 module.exports = {
-  connectionString: process.env.DATABASE_URL
+  // connectionString: process.env.DATABASE_URL
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 }
