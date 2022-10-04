7a6f815425a51e32e1354e0c69eddeb625bde3ee
Kauê Patrick <nullguess.kaue@gmail.com>
2022-10-04 19:21:17 -0300

    Fix connecting to heroku database error

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,12 +1,12 @@
 // Configure pool to connect to the database
 module.exports = {
-  connectionString: process.env.DATABASE_URL,
-  /* user: process.env.PG_USER,
+  // connectionString: process.env.DATABASE_URL,
+  user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT, */
-  /*ssl: {
+  port: process.env.PG_PORT,
+  ssl: {
     rejectUnauthorized: false
-  }*/
+  }
 }
